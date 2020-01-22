Figured out what is wrong with new quaternion lib, will have to do some maths in place of the old library I had

roboBee_class_state_space.py
@@ -291,10 +291,16 @@ class roboBee(object):
         for i in range(3):
             theta_vals[i] = dt*u[9+i] #calculate angle to rotate about orientaiton axes
             if rot_exists:
-                rotation = rotation * Quaternion(axis=self.inertial_frame[i], angle=theta_vals[i])
+                rotation = rotation * np.quaternion(self.inertial_frame[i], theta_vals[i])
             else:
-                rotation = Quaternion(axis=self.inertial_frame[i], angle=theta_vals[i])
+                rotation = np.qaternion(self.inertial_frame[i], theta_vals[i])
                 rot_exists = True
+
+        """
+        LOOK AT MATH OF QUATERNION ROTATIONS TO DO THIS, OR JUST USE ROTATION MATRICES
+        THIS NEW QUATERNION LIBRARY DOES NOT SEEM TO HAVE A 'ROTATE' FUNCTION
+        FOR ROTATING A VECTOR USING A QUATERNION
+        """
 
         if rot_exists:
             u[6:9] = rotation.rotate(u[6:9])

running_robobee.py
@@ -14,7 +14,7 @@ tester = roboBee()
 #tester.updateState()
 #print(tester.orientation)
 
-#tester.run(1200)
+tester.run_analytical(1200)
 
 """
 u_empty = np.zeros(6)
@@ -23,8 +23,9 @@ tester.real_state_space(u_empty, dt)
 """
 
 
-
+"""
 u_test = np.array([0, 0, 0, 10, 0, 0])
 dt = 1/120
 u_des = np.array([0, 0, 10, 10, 0, 0])
 tester.updateState_LQR_Control(u_test, dt, u_des)
+"""
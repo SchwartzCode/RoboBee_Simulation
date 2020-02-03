28674a253e626ea80189dd6cd285706329120abf
SchwartzCode <jonbs98@gmail.com>
2020-02-03 16:07:03 -0500

    Cleaned up some parts of code and set up training data for a neural network to do what the torque controller does

--- running_robobee.py
+++ running_robobee.py
@@ -9,20 +9,16 @@ Created on Mon Sep  9 18:52:30 2019
 from roboBee_class_state_space import *
 
 tester = roboBee()
-#tester.getState()
-#tester.readSensors()
-#tester.updateState()
-#print(tester.orientation)
-
 #tester.run_analytical(1200)
 #tester.run_pd(1200)
-tester.run_pd(400)
 
 """
-u_empty = np.zeros(6)
-dt = 1 / 120
-tester.real_state_space(u_empty, dt)
+JIA: use the 'input' and 'output' variables below to train the neural network, if
+    you want more data, just increaes x in run_pd(x) as that is the number of time
+    steps the simulation will run
 """
+input, output = tester.run_pd(400)
+
 
 
 """

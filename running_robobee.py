9ab7d3e173fa4753729b29b20bc967ba10ca639e
SchwartzCode <jonbs98@gmail.com>
2020-02-12 16:54:18 -0500

    Made it so PD controller generates input and output that is aligned. Nice catch Jia!

--- running_robobee.py
+++ running_robobee.py
@@ -12,14 +12,14 @@ tester = roboBee()
 #tester.run_analytical(1200)
 #tester.run_pd(1200)
 
-tester.run_lqr(500)
+#tester.run_lqr(500)
 
 """
 JIA: use the 'input' and 'output' variables below to train the neural network, if
     you want more data, just increaes x in run_pd(x) as that is the number of time
     steps the simulation will run
 """
-#input, output = tester.run_pd(4000)
+input, output = tester.run_pd(1000)
 
 
 

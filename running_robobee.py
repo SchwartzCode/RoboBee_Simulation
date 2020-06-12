d562c4d89c3126279360bde4de402ae7be8fb4bc
SchwartzCode <jonbs98@gmail.com>
2020-06-12 16:42:32 -0400

    Commented out loops for timing method functions

--- running_robobee.py
+++ running_robobee.py
@@ -5,8 +5,10 @@ Created on Mon Sep  9 18:52:30 2019
 @author: jonbs
 """
 
+import time
+
 #from roboBee_class import *
-from roboBee_class_state_space import *
+from roboBee_class_PD_and_LQR import *
 
 tester = roboBee()
 #tester.run_analytical(1200)
@@ -19,9 +21,11 @@ have values in its second row, as that corresponds to the only state the robot c
 control. I can explain the physics behind this if you'd like, just ask me
 """
 
-input, output = tester.run_lqr(500)
 
-print(input[0].shape, output.shape)
+
+
+
+#print(input[0].shape, output.shape)
 
 
 """
@@ -29,4 +33,34 @@ JIA: use the 'input' and 'output' variables below to train the neural network, i
     you want more data, just increaes x in run_pd(x) as that is the number of time
     steps the simulation will run
 """
-#input, output = tester.run_pd(1000)
+input, output = tester.run_lqr(1000, verbose=True)
+
+"""
+#Timing different method funcs, will probably delete eventually
+
+start = time.time()
+for i in range(2500):
+    print(i)
+end = time.time()
+print("LQR controller: ", end - start)
+
+
+start = time.time()
+for i in range(10):
+    tester.run_lqr(2500)
+end = time.time()
+print("LQR controller: ", end - start)
+
+start = time.time()
+for i in range(10):
+    tester.run_pd(2500)
+end = time.time()
+print("PD controller: ", end - start)
+
+start = time.time()
+for i in range(10):
+    tester.run_analytical(2500)
+end = time.time()
+print("Analytical controller: ", end - start)
+
+"""

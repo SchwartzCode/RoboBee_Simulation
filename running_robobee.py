b8e80db0569298406c66e7a8b6249dce3f377667
Jonathan Schwartz <jonbs98@gmail.com>
2020-06-16 21:50:06 -0400

    Changed some stuff while I was commenting and cleaning up the class to make sure I didn't break anything, will probably archive this soon

--- running_robobee.py
+++ running_robobee.py
@@ -14,14 +14,6 @@ tester = roboBee()
 #tester.run_analytical(1200)
 #tester.run_pd(1200)
 
-"""
-The run_lqr function will return two arrays of size (4, nt) where nt is the integer passed
-to the run_lqr function that determines how many time steps are run. The output array should only
-have values in its second row, as that corresponds to the only state the robot can directly
-control. I can explain the physics behind this if you'd like, just ask me
-"""
-
-
 
 
 
@@ -33,8 +25,8 @@ JIA: use the 'input' and 'output' variables below to train the neural network, i
     you want more data, just increaes x in run_pd(x) as that is the number of time
     steps the simulation will run
 """
-input, output = tester.run_lqr(1000)
-input1, output1 = tester.run_pd(1000)
+input, output = tester.run_lqr(1000, verbose=True)
+#input1, output1 = tester.run_pd(1000)
 
 """
 #Timing different method funcs, will probably delete eventually
